c9cfb6f595bf6d9ad2a7bbf78d47fb81ff110cf3
pwindle <pwindle11@gmail.com>
2018-09-23 22:10:19 +0100

    Update 0.5.8 hash values

--- versions/0.5.8/tools/chocolateyinstall.ps1
+++ versions/0.5.8/tools/chocolateyinstall.ps1
@@ -1,4 +1,4 @@
 ﻿
 $ErrorActionPreference = 'Stop'; # stop on all errors
 
-Install-ChocolateyPackage -packageName 'simplednscrypt' -FileType msi -SilentArgs '/quiet' -Url 'https://github.com/bitbeans/SimpleDnsCrypt/releases/download/0.5.8/SimpleDNSCrypt.msi' -checksum 'D88B099BF013A2358C566259EF3D32A6FD964E9B31F3078B6A8E90F551AAF88C' -checksumType 'sha256' -Url64 'https://github.com/bitbeans/SimpleDnsCrypt/releases/download/0.5.8/SimpleDNSCrypt64.msi' -checksum64 '8D059DEE568FC71DFB49F2830BBD39A5516A497C40333F1038D00FE1C7DADC15' -checksumType64 'sha256' 
+Install-ChocolateyPackage -packageName 'simplednscrypt' -FileType msi -SilentArgs '/quiet' -Url 'https://github.com/bitbeans/SimpleDnsCrypt/releases/download/0.5.8/SimpleDNSCrypt.msi' -checksum '2D12DC18057C9542B163E5E351DA89448E0C4B258575E07C85C1A066A49E7C1D' -checksumType 'sha256' -Url64 'https://github.com/bitbeans/SimpleDnsCrypt/releases/download/0.5.8/SimpleDNSCrypt64.msi' -checksum64 'E8DB2F59B24013891B7CD44B7F6D8906D795881F88A236E72B81E520D5B080A7' -checksumType64 'sha256' 
