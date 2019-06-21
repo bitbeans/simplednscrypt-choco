75a3b0522a84f14e62570c5fd3bd4c048fbd8fb1
Christian Hermann <c.hermann@bitbeans.de>
2019-06-21 09:26:52 +0200

    push 0.6.6

---
+++ versions/0.6.6/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿
+$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+ 
+$packageArgs = @{
+	packageName            = 'simplednscrypt'
+	fileType               = 'MSI'
+	url                    = 'https://github.com/bitbeans/SimpleDnsCrypt/releases/download/0.6.6/SimpleDNSCrypt.msi'
+	url64bit               = 'https://github.com/bitbeans/SimpleDnsCrypt/releases/download/0.6.6/SimpleDNSCrypt64.msi'
+	checksum               = 'E3A288E0201618DD695967BB4D1D28707102366E3227F5536537869B64D1A37C'
+	checksum64             = 'EE5711A2CBF56698CB8D69CBD216C155E916613B2739CBA3338F00AD542C2D1E'
+	checksumType           = 'sha256'
+	checksumType64         = 'sha256'
+	silentArgs             = "/quiet"
+	validExitCodes         = @(0)
+}
+Install-ChocolateyPackage @packageArgs
